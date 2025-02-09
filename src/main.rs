b46dfba33022911e0e39585a0bffa1e283e597a5
Douglas Storm Hill <stormhill117@gmail.com>
2025-02-09 13:50:16 -0600

    Add a note and remove some stale TODOs.

--- src/main.rs
+++ src/main.rs
@@ -39,8 +39,6 @@ use crate::data::{MaybeData, StatusbarData};
 use crate::io::StatusbarIOContext;
 use crate::time::{wait_till_next_minute, wait_till_time_change, ClockChangedCallback};
 
-// TODO: add a place to put realtime clock change detection.
-
 async fn listen_to_upower(
     sys_conn: Arc<LocalConnection>,
     change_q: Sender<StatusbarChangeCause>,
@@ -148,11 +146,6 @@ async fn fire_on_clock_change(
     }
 }
 
-// TODO: async for listening for time change
-// This involves creating a timerfd with TFD_TIMER_CANCEL_ON_SET set,
-// then waiting on it. This will involve tokio::io::AsyncRead, or
-// something like that.
-
 async fn update_statusbar<SBO, DO>(
     mut change_q: Receiver<StatusbarChangeCause>,
     io_ctx: Arc<Mutex<StatusbarIOContext<SBO, DO>>>,
@@ -162,9 +155,6 @@ where
     DO: AsyncWrite + Unpin,
 {
     // TODO this should return Result<!, ...>
-    // TODO: get time
-    // TODO: calculate top of next minute
-    // TODO: sleep until next minute
 
     let mut data = StatusbarData::new();
 
@@ -253,6 +243,7 @@ async fn task_setup() -> Result<(), Box<dyn Error>> {
     let _sys_connect =
         local_tasks.spawn_local(setup_system_connection(sys_conn.clone(), io_ctx.clone()));
 
+    // Make the channel, with a totally arbitrary depth.
     let (tx, rx) = channel(32);
 
     let _upow_connect = local_tasks.spawn_local(listen_to_upower(sys_conn.clone(), tx.clone()));
@@ -262,8 +253,6 @@ async fn task_setup() -> Result<(), Box<dyn Error>> {
     let _listen_adj = local_tasks.spawn_local(fire_on_clock_change(tx));
 
     let _update_stat = local_tasks.spawn_local(update_statusbar(rx, io_ctx));
-
-    // TODO: set up the statusbar printer?
 
     // Wait for our tasks to finish.
     local_tasks.await;
